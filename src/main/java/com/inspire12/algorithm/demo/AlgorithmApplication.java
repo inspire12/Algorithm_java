60cb0c0fe76f0b9729df52912aff1ee153f3ef6e
inspire12 <seo.yeonghak.1992@gmail.com>
2020-06-30 03:11:47 +0900

    leet code: medium 5 https://leetcode.com/problems/longest-palindromic-substring/
    2D cache
    "i" means start point, "k" means interval
    
    #DP

---
+++ src/main/java/com/inspire12/algorithm/demo/AlgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.inspire12.algorithm.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmApplication.class, args);
+    }
+
+}
